37de05c5f66167125ee6b9c3ef71c3bef8c2d457
Ian Overgard <ian.overgard@gmail.com>
2026-03-04 21:07:50 -0600

    Refactor scene data into a SceneManager

--- src/ScenePage.tsx
+++ src/ScenePage.tsx
@@ -7,53 +7,66 @@ import {
   Scene,
 } from "@babylonjs/core";
 import { SceneComponent } from "./SceneComponent"; // uses above component in same directory
-// import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
 import { loadIfcModel } from "./ifc-loader";
+import { useRef } from "react";
 
-let box: Mesh;
+class SceneManager {
+  camera: FreeCamera;
+  displayMesh: Mesh;
 
-const onSceneReady = (scene: Scene) => {
-  // This creates and positions a free camera (non-mesh)
-  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  constructor(private scene: Scene) {
+    // This creates and positions a free camera (non-mesh)
+    this.camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
 
-  // This targets the camera to scene origin
-  camera.setTarget(Vector3.Zero());
+    // This targets the camera to scene origin
+    this.camera.setTarget(Vector3.Zero());
 
-  const canvas = scene.getEngine().getRenderingCanvas();
+    const canvas = scene.getEngine().getRenderingCanvas();
 
-  // This attaches the camera to the canvas
-  camera.attachControl(canvas, true);
+    // This attaches the camera to the canvas
+    this.camera.attachControl(canvas, true);
 
-  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight(
+      "light",
+      new Vector3(0, 1, 0),
+      this.scene,
+    );
 
-  // Default intensity is 1. Let's dim the light a small amount
-  light.intensity = 0.7;
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
 
-  // Our built-in 'box' shape.
-  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+    // Our built-in 'box' shape.
+    this.displayMesh = MeshBuilder.CreateBox("box", { size: 2 }, this.scene);
 
-  // Move the box upward 1/2 its height
-  box.position.y = 1;
+    // Move the box upward 1/2 its height
+    this.displayMesh.position.y = 1;
 
-  // Our built-in 'ground' shape.
-  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-};
+    // Our built-in 'ground' shape.
+    MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, this.scene);
+  }
 
-/**
- * Will run on every frame render.  We are spinning the box on y-axis.
- */
-const onRender = (scene: Scene) => {
-  if (box !== undefined) {
-    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+  onRender() {
+    const deltaTimeInMillis = this.scene.getEngine().getDeltaTime();
 
     const rpm = 10;
-    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+    this.displayMesh.rotation.y +=
+      (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
   }
-};
+}
 
 export function ScenePage() {
+  const sceneMangerRef = useRef<SceneManager | null>(null);
+
+  const onSceneReady = (scene: Scene) => {
+    sceneMangerRef.current = new SceneManager(scene);
+  };
+
+  const onRender = () => {
+    sceneMangerRef.current?.onRender();
+  };
+
   return (
     <>
       <button
