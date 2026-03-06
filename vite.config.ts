0c77016d92c01ee9ab04944011bb6d6fb8bf7616
Ian Overgard <ian.overgard@gmail.com>
2026-03-05 21:47:41 -0600

    Extract mime middleware and fix vite preview server

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Middleware to serve .wasm files with the correct MIME type.
+const wasmMimeMiddleware: import("vite").Connect.NextHandleFunction = (
+  _req,
+  res,
+  next,
+) => {
+  const origSetHeader = res.setHeader.bind(res);
+  res.setHeader = (name, value) => {
+    if (
+      name === "Content-Type" &&
+      typeof value === "string" &&
+      res.req?.url?.endsWith(".wasm")
+    ) {
+      return origSetHeader(name, "application/wasm");
+    }
+    return origSetHeader(name, value);
+  };
+  next();
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
 
-    // Add middleware to serve .wasm files with the correct MIME type.
     // We could use vite-plugin-wasm, but it's kind of overkill
     {
       name: "wasm-mime-type",
       configureServer(server) {
-        server.middlewares.use((_req, res, next) => {
-          const origSetHeader = res.setHeader.bind(res);
-          res.setHeader = (name, value) => {
-            if (
-              name === "Content-Type" &&
-              typeof value === "string" &&
-              res.req?.url?.endsWith(".wasm")
-            ) {
-              return origSetHeader(name, "application/wasm");
-            }
-            return origSetHeader(name, value);
-          };
-          next();
-        });
+        server.middlewares.use(wasmMimeMiddleware);
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use(wasmMimeMiddleware);
       },
     },
   ],
